654e6002f48a6e27efabb287271187dec84a0e5f
Stellakim1230 <stellakim1230@gmail.com>
2020-06-21 17:49:49 +0900

    Fix Random file.

--- random.js
+++ random.js
@@ -4,32 +4,21 @@ const getZeroOrOne = () => {
 
 const getRandom = (number) => {
   var arr = []
+  const count = Math.round(number / 2)
 
-  for (let i = 0; i <= number; i++) {
-    arr.push(parseInt(getBinary(Math.round(i / 2)), 2))
+  for (let i = 0; i < number; i++) {
+    var string = ''
+
+    for (let j = 0; j < count; j++) {
+      string += getZeroOrOne()
+    }
+
+    if (parseInt(string, 2) <= 5) {
+      arr.push(parseInt(string, 2))
+    }
   }
 
   return arr
 }
 
-const getBinary = (times) => {
-  var string = ''
-
-  if (times === 0) return getZeroOrOne()
-
-  for (let i = 1; i <= times; i++) {
-    string += getZeroOrOne()
-  }
-
-  return parseInt(string, 10)
-}
-
-const resultRandom = (number) => {
-  const result = getRandom(number)
-
-  return result.sort(() => {
-    return 0.5 - Math.random()
-  })
-}
-
-console.log(resultRandom(5))
+console.log(getRandom(5))
